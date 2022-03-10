9a6f99eab070b7a9300bc71b00dd6a0c61ee848c
lub34 <lub34@pitt.edu>
2022-03-10 13:32:17 -0500

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,24 @@
 from django.urls import path
 from . import views
 
+"""
+Note: '<int:pk>' allows for a variable appendage to the base url.
+This appendage is a random integer value pulled from the 'pk' argument
+fed to an Django template url call. The 'pk' stands for "primary key".
+"""
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
+
+"""
+Django Workflow (from what I gather after doing DjangoGirls tutorial):
+1. Create a new url path for any branches to your website in 'urls.py' of your website's main directory
+2. Create a respective view for said url in 'views.py' of the same directory.
+3. Add a new template file to your templates sub-directory within the parent directory of your website
+   (i.e. how all templates in this tutorial are under 'blog/templates/blog' rather than simply 'blog'
+   like 'urls.py' and 'views.py' are.)
+4. Use Django's url template to link a url to anchors in HTML code
+5. Use Django block content's to stylize content within a url link's webpage
+"""
 
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post # The '.' in front of 'models' module means "relative/local" library
                          # it's referencing the models.py file in this same directory.
@@ -10,3 +10,7 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk) # Returns a Post object if valid primary key provided, otherwise 404 error page returned
+    return render(request, 'blog/post_detail.html', {'post': post})
